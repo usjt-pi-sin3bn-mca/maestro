5b4550cdc3471ca194e806be5b99cbe94de48db3
Mateus Sampaio <sampa.teco@hotmail.com>
2019-04-28 16:13:28 -0300

    Criando a classe controller para mapear os endpoints

---
+++ src/main/java/br/usjt/apivolei/maestro/controller/ExperienciaController.java
@@ -0,0 +1,35 @@
+package br.usjt.apivolei.maestro.controller;
+
+import br.usjt.apivolei.maestro.model.bean.Experiencia;
+import br.usjt.apivolei.maestro.model.service.ExperienciaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class ExperienciaController {
+
+    @Autowired
+    private ExperienciaService experienciaService;
+
+    @RequestMapping(value = "/experiencia", method = RequestMethod.POST)
+    public ResponseEntity<?> cadastrar(@RequestBody Experiencia experiencia, HttpServletRequest request){
+        return experienciaService.cadastrar(experiencia, request);
+    }
+
+    @RequestMapping(value = "/experiencia/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> buscar(@PathVariable Long id, HttpServletRequest request){
+        return experienciaService.buscar(id, request);
+    }
+
+    @RequestMapping(value = "/experiencia", method = RequestMethod.GET)
+    public ResponseEntity<?> buscarTodas(HttpServletRequest request){
+        return experienciaService.buscar(request);
+    }
+}
