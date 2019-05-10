a6dde3e58d988d8698cc4a7deccf827cc47eef3e
Mateus Sampaio <sampa.teco@hotmail.com>
2019-05-09 21:23:11 -0300

    Alterando a utilizacao da ResponseUtils para ResponseEntity

--- src/main/java/br/usjt/apivolei/maestro/resource/ExperienciaResource.java
+++ src/main/java/br/usjt/apivolei/maestro/resource/ExperienciaResource.java
@@ -10,9 +10,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
-@Controller
+@Resource
 public class ExperienciaController {
 
     @Autowired
