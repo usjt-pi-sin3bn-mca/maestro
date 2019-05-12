a56d73e38fd2c50673bb19815a8a0855ffe84113
juliano <julianoamasp@gmail.com>
2019-05-12 20:14:59 -0300

    repositorio modificado

--- src/main/java/br/usjt/apivolei/maestro/model/repository/ConvenioRepository.java
+++ src/main/java/br/usjt/apivolei/maestro/model/repository/ConvenioRepository.java
@@ -1,9 +1,11 @@
 package br.usjt.apivolei.maestro.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.usjt.apivolei.maestro.model.bean.Convenio;
 
+@Repository
 public interface ConvenioRepository extends JpaRepository< Convenio, Long>{
 
 }
