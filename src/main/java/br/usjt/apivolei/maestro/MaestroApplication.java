83af61d0f4a9bbdabfc74250d6f20a64a242acd9
gb-lucas-duarte <lucas.duarte@guiabolso.com.br>
2019-05-29 22:11:03 -0300

    Adicionado o Cors config no maestro

--- src/main/java/br/usjt/apivolei/maestro/MaestroApplication.java
+++ src/main/java/br/usjt/apivolei/maestro/MaestroApplication.java
@@ -2,6 +2,8 @@ package br.usjt.apivolei.maestro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MaestroApplication {
@@ -10,4 +12,12 @@ public class MaestroApplication {
 		SpringApplication.run(MaestroApplication.class, args);
 	}
 
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("*");
+			}
+		};
+	}
 }
