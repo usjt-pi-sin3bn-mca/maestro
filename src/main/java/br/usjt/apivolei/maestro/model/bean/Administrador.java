c91593470d7f29c43d492ce157ec5d1695694dce
gb-lucas-duarte <lucas.duarte@guiabolso.com.br>
2019-05-29 22:53:53 -0300

    adicionado import generated

--- src/main/java/br/usjt/apivolei/maestro/model/bean/Administrador.java
+++ src/main/java/br/usjt/apivolei/maestro/model/bean/Administrador.java
@@ -2,6 +2,8 @@ package br.usjt.apivolei.maestro.model.bean;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
