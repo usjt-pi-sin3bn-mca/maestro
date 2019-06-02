baa09f1b3653043cb041dd2046299cb5c348dbe8
Renan <renan.viana@vilt-group.com>
2019-06-01 23:07:38 -0300

    removido Optional do método de busca no repository

--- src/main/java/br/usjt/apivolei/maestro/model/repository/AdministradorRepository.java
+++ src/main/java/br/usjt/apivolei/maestro/model/repository/AdministradorRepository.java
@@ -4,9 +4,7 @@ import br.usjt.apivolei.maestro.model.bean.Administrador;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface AdministradorRepository extends JpaRepository<Administrador, Long> {
-    Optional<Administrador> findByEmailAndSenhaAndAtivo(String usuario, String senha, boolean ativo);
+    Administrador findByEmailAndSenha(String usuario, String senha);
 }
